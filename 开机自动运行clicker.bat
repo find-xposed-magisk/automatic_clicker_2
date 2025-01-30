954f4bed19617cb58cb5b482d8b31abfc1f972c4
一只言逸 <9043854+YiZhiYanYi@user.noreply.gitee.com>
2025-01-30 08:14:02 +0000

    删除文件 开机自动运行clicker.bat

--- 开机自动运行clicker.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-start "" "%~dp0clicker.exe"
-ping 127.0.0.1 -n 4 >nul
-echo Set WshShell = CreateObject("WScript.Shell") > send_f10.vbs
-echo WshShell.SendKeys "{F10}" >> send_f10.vbs
-cscript send_f10.vbs
-del send_f10.vbs
